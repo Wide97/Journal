40958a23f0d1baf5ac456a7a29b7a8da76d84eed
Wide97 <mwidesott@gmail.com>
2025-05-09 12:25:22 +0200

    add auth

--- src/main/java/com/marco/tradingjournal/security/CorsConfig.java
+++ src/main/java/com/marco/tradingjournal/security/CorsConfig.java
@@ -13,7 +13,8 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        //.allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("*") // solo per test temporaneo
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
